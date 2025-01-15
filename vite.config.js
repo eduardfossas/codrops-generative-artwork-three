3ef9a47d35cbffcc745b837b77d98f4919b65545
Eduard Fossas <eduardfp@gmail.com>
2025-01-15 15:47:12 +0100

    Update relative path for prod

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './', // Use relative paths
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/Tutorials/GenerativeArtworkThreejs/" : "./",
   plugins: [react()],
-})
+}));
